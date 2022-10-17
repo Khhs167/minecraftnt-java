326e374d42117598d98e4ff457eec0f760a96cae
CheeseCommando <83178164+CheeseCommando@users.noreply.github.com>
2022-10-17 12:44:54 -0400

    Fixed physics

--- game/src/main/java/net/minecraftnt/server/entities/Entity.java
+++ game/src/main/java/net/minecraftnt/server/entities/Entity.java
@@ -1,6 +1,7 @@
 package net.minecraftnt.server.entities;
 
 import net.minecraftnt.server.Minecraft;
+import net.minecraftnt.server.physics.ColliderAABB;
 import net.minecraftnt.server.physics.PhysicsBody;
 import net.minecraftnt.util.Transform;
 import net.minecraftnt.util.Vector3;
@@ -8,6 +9,12 @@ import net.minecraftnt.util.Vector3;
 public abstract class Entity {
     private Transform transform;
     private PhysicsBody physicsBody;
+    
+    protected float boundingBoxSizeX = 0.6f;
+    protected float boundingBoxSizeY = 1.8f;
+    public float eyesOffset = 1.6f;
+    public boolean isGrounded;
+    
     public Transform getTransform(){
         return transform;
     }
@@ -28,9 +35,29 @@ public abstract class Entity {
     
     public Entity(Vector3 pos){
         this.transform = new Transform(pos);
+        setPos(pos);
+    }
+    public void setPos(Vector3 pos) {
+    	float halfWidth = boundingBoxSizeX / 2.0F;
+        float halfHeight = boundingBoxSizeY / 2.0F;
+    	addPhysicsBody().setCollider(new ColliderAABB(
+    			new Vector3(pos.getX() - halfWidth, pos.getY() - halfHeight, pos.getZ() - halfWidth),
+    			new Vector3(pos.getX() + halfWidth, pos.getY() + halfHeight, pos.getZ() + halfWidth)));
     }
     public void update(){
+    	
+    	float originalYVel = getPhysicsBody().getVelocity().getY();
+    	
         getPhysicsBody().updateBody();
+        
+        float YVel = getPhysicsBody().getVelocity().getY();
+        
+        isGrounded = originalYVel != YVel && originalYVel < 0.0F && Math.abs(YVel) < 0.01;
+        
+        ColliderAABB bb = getPhysicsBody().getCollider();
+        transform.location.setX((bb.min.getX() + bb.max.getX()) / 2.0F);
+        transform.location.setY(bb.min.getY());
+        transform.location.setZ((bb.min.getZ() + bb.max.getZ()) / 2.0F);
     }
 
     public float getDeltaTime(){
